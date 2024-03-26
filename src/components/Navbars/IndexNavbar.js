6dd43bfa6593da1086a6a19140bd758facfd22bd
boyaruska <60504713+bayrtsogt@users.noreply.github.com>
2024-03-27 01:58:09 +0800

    push

--- src/components/Navbars/IndexNavbar.js
+++ src/components/Navbars/IndexNavbar.js
@@ -1,25 +1,5 @@
-/*!
-
-=========================================================
-* Paper Kit React - v1.3.2
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/paper-kit-react
-
-* Copyright 2023 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/paper-kit-react/blob/main/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React from "react";
-// nodejs library that concatenates strings
 import classnames from "classnames";
-// reactstrap components
 import {
   Button,
   Collapse,
@@ -28,17 +8,25 @@ import {
   NavItem,
   NavLink,
   Nav,
-  Container,
+  Container, Row, Col,
 } from "reactstrap";
 
 function IndexNavbar() {
   const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
   const [navbarCollapse, setNavbarCollapse] = React.useState(false);
+  const [main, setMain] = React.useState(true);
+  const [profile, setProfile] = React.useState(false);
+  const [sign, setSign] = React.useState(false);
+  const [reg, setReg] = React.useState(false);
 
   const toggleNavbarCollapse = () => {
     setNavbarCollapse(!navbarCollapse);
     document.documentElement.classList.toggle("nav-open");
   };
+  const toProfile = () => {
+    setProfile(true);
+    setMain(false);
+  }
 
   React.useEffect(() => {
     const updateNavbarColor = () => {
@@ -56,107 +44,136 @@ function IndexNavbar() {
     };
 
     window.addEventListener("scroll", updateNavbarColor);
-
+    // setProfile(true);
     return function cleanup() {
       window.removeEventListener("scroll", updateNavbarColor);
     };
   });
   return (
-    <Navbar className={classnames("fixed-top", navbarColor)} expand="lg">
-      <Container>
-        <div className="navbar-translate">
-          <NavbarBrand
-            data-placement="bottom"
-            href="/index"
-            target="_blank"
-            title="Coded by Creative Tim"
-          >
-            Paper Kit Reactdasdfesfw
-          </NavbarBrand>
-          <button
-            aria-expanded={navbarCollapse}
-            className={classnames("navbar-toggler navbar-toggler", {
-              toggled: navbarCollapse,
-            })}
-            onClick={toggleNavbarCollapse}
-          >
-            <span className="navbar-toggler-bar bar1" />
-            <span className="navbar-toggler-bar bar2" />
-            <span className="navbar-toggler-bar bar3" />
-          </button>
-        </div>
-        <Collapse
-          className="justify-content-end"
-          navbar
-          isOpen={navbarCollapse}
+      <>
+        <Navbar className={classnames("fixed-top", navbarColor)} expand="lg">
+          <Container>
+            <div className="navbar-translate">
+              <NavbarBrand
+                  data-placement="bottom"
+                  title="Coded by Creative Tim"
+              >
+                Century Car
+              </NavbarBrand>
+              <button
+                  aria-expanded={navbarCollapse}
+                  className={classnames("navbar-toggler navbar-toggler", {
+                    toggled: navbarCollapse,
+                  })}
+                  onClick={toggleNavbarCollapse}
+              >
+                <span className="navbar-toggler-bar bar1" />
+                <span className="navbar-toggler-bar bar2" />
+                <span className="navbar-toggler-bar bar3" />
+              </button>
+            </div>
+            <Collapse
+                className="justify-content-end"
+                navbar
+                isOpen={navbarCollapse}
+            >
+              <Nav navbar>
+                <NavItem>
+                  <NavLink onClick={() => {toProfile()}}>
+                    Бидний тухай
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink
+                      to="/landing-page"
+                  >
+                    Нэвтрэх
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <Button
+                      className="btn-round"
+                      color="info"
+                      href="/register-page"
+                  >
+                    Бүртгүүлэх
+                  </Button>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+
+        </Navbar>
+        <div
+            className="page-header section-dark"
+            style={{
+              backgroundImage:
+                  "url(" + require("assets/img/background.jpg") + ")",
+            }}
         >
-          <Nav navbar>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://twitter.com/CreativeTim?ref=creativetim"
-                target="_blank"
-                title="Follow us on Twitter"
-              >
-                <i className="fa fa-twitter" />
-                <p className="d-lg-none">Twitter</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.facebook.com/CreativeTim?ref=creativetim"
-                target="_blank"
-                title="Like us on Facebook"
-              >
-                <i className="fa fa-facebook-square" />
-                <p className="d-lg-none">Facebook</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.instagram.com/CreativeTimOfficial?ref=creativetim"
-                target="_blank"
-                title="Follow us on Instagram"
-              >
-                <i className="fa fa-instagram" />
-                <p className="d-lg-none">Instagram</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                data-placement="bottom"
-                href="https://www.github.com/CreativeTimOfficial/paper-kit-react?ref=creativetim"
-                target="_blank"
-                title="Star on GitHub"
-              >
-                <i className="fa fa-github" />
-                <p className="d-lg-none">GitHub</p>
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink
-                href="https://demos.creative-tim.com/paper-kit-react/#/documentation?ref=pkr-index-navbar"
-                target="_blank"
-              >
-                <i className="nc-icon nc-book-bookmark" /> Documentation
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <Button
-                className="btn-round"
-                color="danger"
-                href="https://www.creative-tim.com/product/paper-kit-pro-react?ref=pkr-index-navbar"
-                target="_blank"
-              >
-                <i className="nc-icon nc-spaceship"></i> Upgrade to Pro
-              </Button>
-            </NavItem>
-          </Nav>
-        </Collapse>
-      </Container>
-    </Navbar>
+          <div className="filter" />
+          <div className="content-center">
+            {main ?
+            <Container>
+              <div className="title-brand">
+                <h1 className="presentation-title">Century Car</h1>
+                {/*<div className="fog-low">*/}
+                {/*  <img alt="..." src={require("assets/img/fog-low.png")} />*/}
+                {/*</div>*/}
+                {/*<div className="fog-low right">*/}
+                {/*  <img alt="..." src={require("assets/img/fog-low.png")} />*/}
+                {/*</div>*/}
+              </div>
+              <h2 className="presentation-subtitle text-center">
+                Баталгаат машин худалдааны цогц системд тавтай морил!
+              </h2>
+
+            </Container>
+            : null}
+
+            {profile && (<Container>
+              <div className="owner">
+                <div className="avatar">
+                  <img
+                      alt="..."
+                      className="img-circle img-no-padding img-responsive"
+                      src={require("assets/img/faces/joe-gardner-2.jpg")}
+                  />
+                </div>
+                <div className="name">
+                  <h4 className="title">
+                    Jane Faker <br />
+                  </h4>
+                  <h6 className="description">Music Producer</h6>
+                </div>
+              </div>
+              <Row>
+                <Col className="ml-auto mr-auto text-center" md="6">
+                  <p>
+                    An artist of considerable range, Jane Faker — the name taken by
+                    Melbourne-raised, Brooklyn-based Nick Murphy — writes, performs
+                    and records all of his own music, giving it a warm, intimate
+                    feel with a solid groove structure.
+                  </p>
+                </Col>
+              </Row>
+            </Container>)
+            }
+
+
+
+          </div>
+          <div
+              className="moving-clouds"
+              style={{
+                backgroundImage: "url(" + require("assets/img/clouds.png") + ")",
+              }}
+          />
+          <h6 className="category category-absolute">
+            fast buy
+          </h6>
+        </div>
+      </>
   );
 }
 
--- src/views/examples/ProfilePage.js
+++ src/views/examples/ProfilePage.js
@@ -38,6 +38,7 @@ import {
 import ExamplesNavbar from "components/Navbars/ExamplesNavbar.js";
 import ProfilePageHeader from "components/Headers/ProfilePageHeader.js";
 import DemoFooter from "components/Footers/DemoFooter.js";
+import IndexNavbar from "../../components/Navbars/IndexNavbar";
 
 function ProfilePage() {
   const [activeTab, setActiveTab] = React.useState("1");
@@ -57,7 +58,7 @@ function ProfilePage() {
   });
   return (
     <>
-      <ExamplesNavbar />
+      <IndexNavbar />
       <ProfilePageHeader />
       <div className="section profile-content">
         <Container>
@@ -84,111 +85,8 @@ function ProfilePage() {
                 and records all of his own music, giving it a warm, intimate
                 feel with a solid groove structure.
               </p>
-              <br />
-              <Button className="btn-round" color="default" outline>
-                <i className="fa fa-cog" /> Settings
-              </Button>
             </Col>
           </Row>
-          <br />
-          <div className="nav-tabs-navigation">
-            <div className="nav-tabs-wrapper">
-              <Nav role="tablist" tabs>
-                <NavItem>
-                  <NavLink
-                    className={activeTab === "1" ? "active" : ""}
-                    onClick={() => {
-                      toggle("1");
-                    }}
-                  >
-                    Follows
-                  </NavLink>
-                </NavItem>
-                <NavItem>
-                  <NavLink
-                    className={activeTab === "2" ? "active" : ""}
-                    onClick={() => {
-                      toggle("2");
-                    }}
-                  >
-                    Following
-                  </NavLink>
-                </NavItem>
-              </Nav>
-            </div>
-          </div>
-          {/* Tab panes */}
-          <TabContent className="following" activeTab={activeTab}>
-            <TabPane tabId="1" id="follows">
-              <Row>
-                <Col className="ml-auto mr-auto" md="6">
-                  <ul className="list-unstyled follows">
-                    <li>
-                      <Row>
-                        <Col className="ml-auto mr-auto" lg="2" md="4" xs="4">
-                          <img
-                            alt="..."
-                            className="img-circle img-no-padding img-responsive"
-                            src={require("assets/img/faces/clem-onojeghuo-2.jpg")}
-                          />
-                        </Col>
-                        <Col className="ml-auto mr-auto" lg="7" md="4" xs="4">
-                          <h6>
-                            Flume <br />
-                            <small>Musical Producer</small>
-                          </h6>
-                        </Col>
-                        <Col className="ml-auto mr-auto" lg="3" md="4" xs="4">
-                          <FormGroup check>
-                            <Label check>
-                              <Input
-                                defaultChecked
-                                defaultValue=""
-                                type="checkbox"
-                              />
-                              <span className="form-check-sign" />
-                            </Label>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                    </li>
-                    <hr />
-                    <li>
-                      <Row>
-                        <Col className="mx-auto" lg="2" md="4" xs="4">
-                          <img
-                            alt="..."
-                            className="img-circle img-no-padding img-responsive"
-                            src={require("assets/img/faces/ayo-ogunseinde-2.jpg")}
-                          />
-                        </Col>
-                        <Col lg="7" md="4" xs="4">
-                          <h6>
-                            Banks <br />
-                            <small>Singer</small>
-                          </h6>
-                        </Col>
-                        <Col lg="3" md="4" xs="4">
-                          <FormGroup check>
-                            <Label check>
-                              <Input defaultValue="" type="checkbox" />
-                              <span className="form-check-sign" />
-                            </Label>
-                          </FormGroup>
-                        </Col>
-                      </Row>
-                    </li>
-                  </ul>
-                </Col>
-              </Row>
-            </TabPane>
-            <TabPane className="text-center" tabId="2" id="following">
-              <h3 className="text-muted">Not following anyone yet :(</h3>
-              <Button className="btn-round" color="warning">
-                Find artists
-              </Button>
-            </TabPane>
-          </TabContent>
         </Container>
       </div>
       <DemoFooter />
